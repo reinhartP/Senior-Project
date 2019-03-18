556637b492e5b38050c60ff633be46e803e499ff
reinhartP <reinhartjp@gmail.com>
2019-03-17 23:52:16 -0700

    added google login and temp front end

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+    'googleAuth' : {
+        'clientID'      : process.env.GOOGLE_CLIENT_ID,
+        'clientSecret'  : process.env.GOOGLE_CLIENT_SECRET,
+        'callbackURL'   : process.env.GOOGLE_CALLBACK_URL,
+    }
+};
