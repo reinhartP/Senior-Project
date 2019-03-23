42e1ef2bbfd0c10f8929dfcb1d900ddc01fbf7af
reinhartP <reinhartjp@gmail.com>
2019-03-22 21:47:55 -0700

    updated playlist model to have total songs and spotify id

--- models/playlist.js
+++ models/playlist.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, Sequelize) => {
         userId: {
             type: Sequelize.STRING,
             allowNull: false,
+        },
+        numberOfSongs: {
+            type: Sequelize.INTEGER,
         }
     };
     let modelOptions = {
