2eace08f5653bc9e5e54926cb0305e92651e217c
reinhartP <reinhartjp@gmail.com>
2019-05-08 03:37:40 -0700

    removed unused endpoints

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,12 +3,11 @@ let authController = require("../controllers/authController");
 let configAuth = require("../../config/auth");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
+const path = require('path')
 // app/routes.js
 module.exports = function(app) {
-    app.get("/", function(req, res) {
-        //home page
-        res.render("index.ejs"); // load the index.ejs file
-    });
+    let User = models.user;
+    
 
     // LOGIN
     app.post("/login", (req, res, next) => {
@@ -23,11 +22,11 @@ module.exports = function(app) {
                 req.login(user, err => {
                     User.findOne({
                         where: {
-                            email: user.email,
+                            username: user.username,
                         },
                     }).then(user => {
                         const token = jwt.sign(
-                            { id: user.email },
+                            { id: user.username },
                             configAuth.jwtSecret
                         );
                         res.status(200).send({
@@ -100,68 +99,7 @@ module.exports = function(app) {
     });
 
     // LOGOUT
-    app.get("/logout", authController.logout);
-
-    // GOOGLE ROUTES
-    app.get(
-        "/auth/google",
-        passport.authenticate("google", {
-            scope: ["profile", "email"], //send to google to authenticate
-        })
-    );
-    //profile gets basic information including name
-    //email gets their email
-
-    app.get(
-        "/auth/google/callback", //callback after google has authenticated the user
-        passport.authenticate("google", {
-            successRedirect: "/profile",
-            failureRedirect: "/",
-        })
-    );
-
-    // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT)
-    // connect local account
-    app.get("/connect/local", authController.connect);
-    app.post(
-        "/connect/local",
-        passport.authenticate("local-signup", {
-            successRedirect: "/profile", // redirect to the secure profile section
-            failureRedirect: "/connect/local", // redirect back to the signup page if there is an error
-            failureFlash: true, // allow flash messages
-        })
-    );
-
-    // connect google account
-    app.get(
-        "/connect/google",
-        passport.authorize("google", { scope: ["profile", "email"] })
-    );
-
-    app.get(
-        "/connect/google/callback", // the callback after google has authorized the user
-        passport.authorize("google", {
-            successRedirect: "/profile",
-            failureRedirect: "/",
-        })
-    );
-
-    // UNLINK ACCOUNTS
-    //used to unlink accounts. for social accounts, just remove the token
-    let User = models.user;
-    //local
-    /*app.get('/unlink/local', (req, res) => {      //removed ability to unlink local account
-        let user = req.user;
-        User.findByPk(user.id).then(newUser => {
-            newUser.update({
-                email:       null,
-                password:    null,
-            }).then(() => res.redirect('/profile'));
-        });
-    });*/
-
-    //google
-    app.get("/unlink/google", authController.unlinkGoogle);
+    //app.get("/logout", authController.logout);
 
     //spotify
     app.get("/spotify/sync", isLoggedIn, authController.spotify);
@@ -169,8 +107,6 @@ module.exports = function(app) {
     app.get("/spotify/callback", isLoggedIn, authController.spotifyCallback);
 
     app.get("/spotify/playlist", isLoggedIn, authController.spotifyPlaylist);
-
-    app.get("/youtube", authController.youtube);
 
     app.get("/api/youtube/top", authController.youtube);
 
@@ -181,6 +117,11 @@ module.exports = function(app) {
     app.post("/api/test", authController.spotifySyncPlaylist); //sync songs/artists of a playlist
     app.get("/api/user/playlists", authController.getPlaylistSongs); //returns all of the playlists and the songs in the playlist for a user
     //right now just returns for user test@test.com
+
+    app.get("*", function(req, res) {
+        //home page
+        res.sendFile(path.join(__dirname, '../../../frontend/build/index.html'))
+    });
 };
 
 // route middleware to make sure a user is logged in
