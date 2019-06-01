5539172f13d0dbe401c326ff8cc29a59efd0fa7c
reinhartP <reinhartjp@gmail.com>
2019-05-31 17:37:04 -0700

    added prerender-node for seo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const compression = require('compression');
 const models = require('./models');
 const env = require('dotenv').config(__dirname + './env');
 const app = express();
@@ -19,15 +19,23 @@ models.sequelize
     .catch(err => console.log(err, 'Something went wrong'));
 
 require('./config/passport');
-
+app.use(
+    require('prerender-node')
+        .set('prerenderToken', 'S5D3CMECgzJ1yST0fOYu')
+        .set('protocol', 'https')
+        .set('host', 'www.paghunie.com')
+);
 app.use(cors());
+app.use(compression());
 //set up express application
 app.use(morgan('dev')); //log every request to the console
 app.use(bodyParser.urlencoded({ extended: false })); //get information from html forms
 app.use(bodyParser.json());
 app.use(
     '/static',
-    express.static(path.join(__dirname, './client/build//static'))
+    express.static(path.join(__dirname, '../frontend/build//static'), {
+        maxAge: '1m',
+    })
 );
 //app.use(express.static(path.join(__dirname + '../frontend/build')));
 
