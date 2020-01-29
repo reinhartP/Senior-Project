345830306b294fc11397e0d6b3b31388d9341676
reinhartP <reinhartjp@gmail.com>
2020-01-29 02:03:12 -0800

    testing gcp sql

--- config/database.js
+++ config/database.js
@@ -1,59 +1,68 @@
-require('dotenv').config();
 const fs = require('fs');
-// const rdsCa = fs.readFileSync(__dirname + '/rds-combined-ca-bundle.pem');
-// const gcpCa = fs.readFileSync(__dirname + '/server-ca.pem');
-// const gcpCert = fs.readFileSync(__dirname + '/client-cert.pem');
-// const gcpKey = fs.readFileSync(__dirname + '/client-key.pem');
-// const development = {
-//     username: process.env.DATABASE_USERNAME,
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_NAME,
-//     host: process.env.DATABASE_HOST,
-//     port: '3306',
-//     dialect: 'mysql',
-//     define: {
-//         underscored: true,
-//     },
-//     dialectOptions: {
-//         ssl: {
-//             rejectUnauthorized: true,
-//             ca: [rdsCa],
-//         },
-//     },
-//     logging: false,
-//     pool: {
-//         max: 50,
-//         min: 0,
-//         idle: 10000,
-//         acquire: 50000,
-//     },
-// };
-// const test = {
-//     username: 'root',
-//     password: process.env.DATABASE_PASSWORD,
-//     database: process.env.DATABASE_NAME,
-//     host: '35.230.76.188',
-//     port: '3306',
-//     dialect: 'mysql',
-//     define: {
-//         underscored: true,
-//     },
-//     dialectOptions: {
-//         ssl: {
-//             rejectUnauthorized: true,
-//             ca: [gcpCa],
-//             key: [gcpKey],
-//             cert: [gcpCert],
-//         },
-//     },
-//     logging: false,
-//     pool: {
-//         max: 50,
-//         min: 0,
-//         idle: 10000,
-//         acquire: 50000,
-//     },
-// };
+let rdsCa;
+let gcpCa;
+let gcpCert;
+let gcpKey;
+if (process.env.NODE_ENV !== 'production') {
+    rdsCa = fs.readFileSync(__dirname + '/rds-combined-ca-bundle.pem');
+    gcpCa = fs.readFileSync(__dirname + '/server-ca.pem');
+    gcpCert = fs.readFileSync(__dirname + '/client-cert.pem');
+    gcpKey = fs.readFileSync(__dirname + '/client-key.pem');
+}
+
+const development = {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: '3306',
+    dialect: 'mysql',
+    define: {
+        underscored: true,
+    },
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,
+            //ca: [rdsCa],
+            ca: [gcpCa],
+            key: [gcpKey],
+            cert: [gcpCert],
+        },
+    },
+    logging: false,
+    pool: {
+        max: 50,
+        min: 0,
+        idle: 10000,
+        acquire: 50000,
+    },
+};
+const test = {
+    username: 'root',
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: '3306',
+    dialect: 'mysql',
+    define: {
+        underscored: true,
+    },
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,
+            ca: [gcpCa],
+            key: [gcpKey],
+            cert: [gcpCert],
+        },
+    },
+    logging: false,
+    pool: {
+        max: 50,
+        min: 0,
+        idle: 10000,
+        acquire: 50000,
+    },
+};
 
 const production = {
     username: 'root',
@@ -83,12 +92,9 @@ const production = {
     },
 };
 
-// const config = {
-//     development,
-//     test,
-//     production,
-// };
 const config = {
+    development,
+    test,
     production,
 };
 module.exports = config;
